3cfb15ac5382d49f9812a2ca52437d2e44103174
Manikannan Marimuthu <manikannanmarimuthu@gmail.com>
2023-01-28 10:05:52 +0530

    updating ./roboshob-jenkins-shared-library

---
+++ vars/test1.groovy
@@ -0,0 +1,25 @@
+def new1(){
+    print "hello"
+    def xyz = "hello"
+    print "xyz = ${xyz}"
+    print "abc = ${abc}"
+
+    if (abc=="Some data"){
+        print "YES"
+    }else {
+        print "NO"
+    }
+
+def x = 2;
+    def y = 0;
+    while (x>y){
+        println"${y}"
+        y++
+    }
+
+
+    def fruits = ["apple","banana","orange"];
+    for(i in fruits){
+        println(i);
+    }
+}
