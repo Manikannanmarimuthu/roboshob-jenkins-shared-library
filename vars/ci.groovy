d86e2425fa367730a5197086bfa7a24d7df8e980
Manikannan Marimuthu <manikannanmarimuthu@gmail.com>
2023-02-16 07:14:54 +0530

    updating ./roboshob-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -5,7 +5,6 @@ def call(){
             stage('Checkout') {
                 cleanWs()
                 git branch: 'main', url: "https://github.com/Manikannanmarimuthu/cart"
-               // sh 'env'
             }
 
             stage('Compile/Bild') {
@@ -17,8 +16,8 @@ def call(){
             }
 
             stage('Quality Control') {
-                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_PASS = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                     sh "sonar-scanner -Dsonar.host.url=http://54.160.14.249:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
                 }
